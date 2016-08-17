c7c502b2cd467658d893a9e9418733985efcffa8
Tuan Pham Ngoc <github@joomdonation.com>
2016-08-17 14:56:12 +0700

    Sample code to show you how to use JTable to perform common tasks

--- admin/code.php
+++ admin/code.php
@@ -11,4 +11,54 @@
 // no direct access
 defined('_JEXEC') or die;
 
-echo 'Hello Worlds !';
+echo '<h2>JTable Method</h2>';
+
+// Init JTable object
+$row = JTable::getInstance('category', 'CodeTable');
+
+// Load a record from database
+$row->load(1);
+echo '<pre>';
+echo $row->id . ':' . $row->title . ':' . $row->description;
+echo '</pre>';
+
+// Update data of that record
+$row->title       = 'New Title';
+$row->description = 'New description';
+$row->store();
+
+// Insert new record
+$row              = JTable::getInstance('item', 'CodeTable');
+$row->title       = 'New Record Title';
+$row->description = 'New Record Description';
+$row->store();
+
+echo '<pre>';
+echo 'ID of new record:' . $row->id;
+echo '</pre>';
+
+// Insert new record use bind method
+$row  = JTable::getInstance('item', 'CodeTable');
+$data = array(
+	'title'       => 'Bind Method',
+	'category_id' => 1,
+	'title'       => 'New Record Created By Bind Method',
+	'ordering'    => 1,
+	'published'   => 1
+);
+$row->bind($data);
+$row->store();
+
+// Delete this record, uncomment the code below to see how it works
+//$row->delete();
+
+
+
+
+
+
+
+
+
+
+
